a07a01d13504c7ff0460bcada86b2a6ac4401fb2
mani3354488 <mani3354488@gmail.com>
2020-05-28 23:17:52 +0530

    like button rendered

--- tweetme/tweets/views.py
+++ tweetme/tweets/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+import random
 
 def Home_View(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all() #list of django objects
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 100)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
