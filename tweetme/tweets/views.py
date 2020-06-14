d90a43e1d28d96df68fa5a728fafbe82bc46b7e2
mani3354488 <mani3354488@gmail.com>
2020-06-14 13:49:27 +0530

    changed to restframework

--- tweetme/tweets/views.py
+++ tweetme/tweets/views.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
- 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
 from .serializers import TweetSerializer
@@ -13,13 +15,13 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
-
+@api_view(['POST']) #declare list of methods to support by
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST or None)
-    if serializer.is_valid():
+    if serializer.is_valid(raise_exception=True):
         obj = serializer.save(user=request.user) 
-        return JsonResponse(serializer.data, status=201)
-    return JsonResponse({}, status=400)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
 
 def tweet_create_puredj_view(request, *args, **kwargs):
     user = request.user
@@ -44,9 +46,14 @@ def tweet_create_puredj_view(request, *args, **kwargs):
         if request.is_ajax():
             return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
- 
 
+@api_view(['GET']) 
 def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data)
+
+def tweet_list_view_puredj(request, *args, **kwargs):
     """
     REST API VIEW
     Consume by JavaScript or Swift/Java/iOS/Andriod
@@ -61,7 +68,16 @@ def tweet_list_view(request, *args, **kwargs):
     return JsonResponse(data)
 
 
+@api_view(['GET']) 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data)
+
+def tweet_detail_view_puredj(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
     Consume by JavaScript or Swift/Java/iOS/Andriod
