05b4c3b79d6a151b89b005e058ce0d58f953f49a
mani3354488 <mani3354488@gmail.com>
2020-07-01 14:21:23 +0530

    csrf and client side action

--- tweetme/tweets/views.py
+++ tweetme/tweets/views.py
@@ -119,7 +119,7 @@ def tweet_action_view(request, *args, **kwargs):
         id is required
         tweets action are like, unlike, retweet
     '''
-    serializer = TweetActionSerializer(request.POST)
+    serializer = TweetActionSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
         data = serializer.validate_data
         tweet_id = data.get("id")
